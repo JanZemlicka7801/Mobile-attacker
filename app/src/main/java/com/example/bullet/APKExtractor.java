4b8bfcdb7cb86888f97f250d978125e422af79cf
Jan Zemlicka <zemlicka@mobisec.com>
2024-07-24 14:49:34 +0200

    Just a minor update to see which packages are installed on the device, when the class will slide to a catch.

--- app/src/main/java/com/example/bullet/APKExtractor.java
+++ app/src/main/java/com/example/bullet/APKExtractor.java
@@ -10,6 +10,7 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.List;
 
 public class APKExtractor {
     // TAG for logging purposes
@@ -28,16 +29,18 @@ public class APKExtractor {
             PackageManager pm = context.getPackageManager();
 
             // Retrieve ApplicationInfo for the specified package
-            ApplicationInfo appInfo = pm.getApplicationInfo(packageName, 0);
+            ApplicationInfo appInfo = pm.getApplicationInfo(packageName, PackageManager.GET_META_DATA);
 
             // Get the source directory of the APK file
             String sourceDir = appInfo.sourceDir;
+            Log.d(TAG, "Source directory of APK: " + sourceDir);
 
             // Create input stream from the APK source file
             InputStream in = new FileInputStream(new File(sourceDir));
 
             // Create output file in the external files directory
             File outFile = new File(context.getExternalFilesDir(null), packageName + ".apk");
+            Log.d(TAG, "Output file path: " + outFile.getAbsolutePath());
 
             // Create output stream to the new file
             OutputStream out = new FileOutputStream(outFile);
@@ -57,10 +60,29 @@ public class APKExtractor {
 
             // Return the extracted APK file
             return outFile;
+        } catch (PackageManager.NameNotFoundException e) {
+            Log.e(TAG, "Package name not found: " + packageName, e);
+            logInstalledPackages(context);
+        } catch (SecurityException e) {
+            Log.e(TAG, "Permission denied for package: " + packageName, e);
         } catch (Exception e) {
-            // Log any exceptions that occur during extraction
-            Log.e(TAG, "Failed to extract APK", e);
-            return null;
+            // Log any other exceptions that occur during extraction
+            Log.e(TAG, "Failed to extract APK for package: " + packageName, e);
+        }
+        return null;
+    }
+
+    /**
+     * Log all installed packages to help debug package name issues.
+     *
+     * @param context The context of the application.
+     */
+    private static void logInstalledPackages(Context context) {
+        PackageManager pm = context.getPackageManager();
+        List<ApplicationInfo> packages = pm.getInstalledApplications(PackageManager.GET_META_DATA);
+        Log.d(TAG, "Installed packages:");
+        for (ApplicationInfo packageInfo : packages) {
+            Log.d(TAG, "Package: " + packageInfo.packageName + " | SourceDir: " + packageInfo.sourceDir);
         }
     }
 }
