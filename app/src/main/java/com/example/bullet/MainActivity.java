0b4eb890a245a88c8f6b496ac6728601e1654f17
Jan Zemlicka <zemlicka@mobisec.com>
2024-07-25 11:20:10 +0200

    Deleted unused class and also delete extracting the apk as it is not necessary.

--- app/src/main/java/com/example/bullet/MainActivity.java
+++ app/src/main/java/com/example/bullet/MainActivity.java
@@ -2,16 +2,13 @@ package com.example.bullet;
 
 import android.Manifest;
 import android.content.pm.ActivityInfo;
-import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.ProviderInfo;
 import android.content.pm.ServiceInfo;
 import android.os.Build;
 import android.os.Bundle;
-import android.os.Environment;
 import android.util.Log;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -23,10 +20,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
@@ -44,26 +37,12 @@ public class MainActivity extends AppCompatActivity {
         requestPermissions();
 
         EditText editTextPackageName = findViewById(R.id.editTextPackageName);
-        Button btnExtract = findViewById(R.id.btnExtract);
         Button btnFetchIPC = findViewById(R.id.btnFetchIPC);
         listViewIPC = findViewById(R.id.listViewIPC);
 
         ipcList = new ArrayList<>();
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, ipcList);
         listViewIPC.setAdapter(adapter);
-
-        btnExtract.setOnClickListener(view -> {
-            String packageName = editTextPackageName.getText().toString().trim();
-            if (!packageName.isEmpty()) {
-                if (checkPermissions()) {
-                    extractAndSaveApk(packageName);
-                } else {
-                    Toast.makeText(this, "Permissions are not granted", Toast.LENGTH_SHORT).show();
-                }
-            } else {
-                Toast.makeText(this, "Please enter a package name", Toast.LENGTH_SHORT).show();
-            }
-        });
 
         btnFetchIPC.setOnClickListener(view -> {
             String packageName = editTextPackageName.getText().toString().trim();
@@ -78,7 +57,6 @@ public class MainActivity extends AppCompatActivity {
             String selectedItem = ipcList.get(position);
             Toast.makeText(this, "Selected: " + selectedItem, Toast.LENGTH_SHORT).show();
             // Perform further actions with the selected item here
-            // For example, you could start a new activity, display more details, etc.
         });
     }
 
@@ -91,60 +69,6 @@ public class MainActivity extends AppCompatActivity {
                 ActivityCompat.requestPermissions(this,
                         new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE},
                         PERMISSION_REQUEST_CODE);
-            }
-        }
-    }
-
-    private boolean checkPermissions() {
-        return ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED &&
-                ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED;
-    }
-
-    private void extractAndSaveApk(String packageName) {
-        PackageManager packageManager = getPackageManager();
-        try {
-            ApplicationInfo packageInfo = packageManager.getApplicationInfo(packageName, PackageManager.GET_META_DATA);
-            String sourceDir = packageInfo.sourceDir;
-            File sourceFile = new File(sourceDir);
-            String state = Environment.getExternalStorageState();
-            if (Environment.MEDIA_MOUNTED.equals(state)) {
-                File externalFilesDir = getExternalFilesDir(null);
-                File dir = new File(externalFilesDir, "AppExtractor");
-                Log.d(TAG, "Attempting to create directory: " + dir.getAbsolutePath());
-                if (!dir.exists()) {
-                    boolean created = dir.mkdirs();
-                    if (!created) {
-                        Log.e(TAG, "Failed to create directory: " + dir.getAbsolutePath());
-                        Toast.makeText(this, "Failed to create directory", Toast.LENGTH_SHORT).show();
-                        return;
-                    }
-                }
-                File destFile = new File(dir, packageInfo.packageName + ".apk");
-                try {
-                    copyFile(sourceFile, destFile);
-                    Log.i(TAG, "Successfully stored: " + destFile.getAbsolutePath() + " for package: " + packageName);
-                    Toast.makeText(this, "APK stored at: " + destFile.getAbsolutePath(), Toast.LENGTH_LONG).show();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                    Toast.makeText(this, "Failed to store APK", Toast.LENGTH_SHORT).show();
-                }
-            } else {
-                Log.e(TAG, "SD card not mounted");
-                Toast.makeText(this, "SD card not mounted", Toast.LENGTH_SHORT).show();
-            }
-        } catch (PackageManager.NameNotFoundException e) {
-            e.printStackTrace();
-            Toast.makeText(this, "Package not found", Toast.LENGTH_SHORT).show();
-        }
-    }
-
-    private void copyFile(File sourceFile, File destFile) throws IOException {
-        try (FileInputStream in = new FileInputStream(sourceFile);
-             FileOutputStream out = new FileOutputStream(destFile)) {
-            byte[] buffer = new byte[1024];
-            int length;
-            while ((length = in.read(buffer)) > 0) {
-                out.write(buffer, 0, length);
             }
         }
     }
@@ -168,7 +92,7 @@ public class MainActivity extends AppCompatActivity {
 
             if (packageInfo.activities != null) {
                 for (ActivityInfo activityInfo : packageInfo.activities) {
-                    if (activityInfo.exported && !(activityInfo.name.contains("com.android.app.MainActivity"))) {
+                    if (activityInfo.exported) {
                         ipcList.add("Activity: " + activityInfo.name);
                     }
                 }
